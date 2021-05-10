52c4fe6fc7bbb56859fd9403c437cb2dbe099985
xld1393679430 <x1393679430@outlook.com>
2021-05-10 15:36:37 +0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import dva from 'dva';
+import './utils/flexible';
+
+console.log('test7')
+// 1. Initialize
+const app = dva();
+
+// 2. Plugins
+// app.use({});
+
+// 3. Model
+app.model(require('./models/leftPage').default);
+app.model(require('./models/centerPage').default);
+app.model(require('./models/rightPage').default);
+
+// 4. Router
+app.router(require('./router').default);
+
+// 5. Start
+app.start('#root');
